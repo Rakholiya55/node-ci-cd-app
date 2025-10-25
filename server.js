6e0941a06585b99effff56ee5e7379172d24eabd
Dipkumar Rakholiya <deeprakholiya164@gmail.com>
2025-10-25 14:30:33 -0400

    Update homepage message

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express=require('express');
-const app=express();
-const PORT=3000;
+const express = require("express");
+const app = express();
+const PORT = 3000;
 
-app.get('/', (req, res)=> {
-    res.send('<h1>Hello from CI/CD Node app!</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from CI/CD Node app Deep!</h1>");
 });
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
